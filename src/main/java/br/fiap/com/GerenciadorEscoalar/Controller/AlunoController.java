c8cd7603248d5e66fe8f7c4485f1cf5beecbce52
alef <lafitealef@gmail.com>
2024-03-03 11:44:20 -0300

    Atualização projeto dia 03/03

--- src/main/java/br/fiap/com/GerenciadorEscoalar/Controller/AlunoController.java
+++ src/main/java/br/fiap/com/GerenciadorEscoalar/Controller/AlunoController.java
@@ -1,26 +1,26 @@
 package br.fiap.com.GerenciadorEscoalar.Controller;
 
-import java.net.http.HttpRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class CategoriaController {
-    @RequestMapping(method = RequestMethod.GET, path = "/Categoria", produces = "application/json")
+public class AlunoController {
+    @RequestMapping(method = RequestMethod.GET, path = "/Aluno", produces = "application/json")
     @ResponseBody
     public String index(){
         return """
                 [
                     {
-                        "id":1,
-                        "nome":'Alimentação',
-                        "icone":"fast-food"
+                        "rm":0000000,
+                        "nome":'Joao',
+                        "turma": 'A'
                     }
                 ]
                 """;
     }
+    
+
     
 }
