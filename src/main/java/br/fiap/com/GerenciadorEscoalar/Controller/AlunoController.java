bbdeef285d15b959faa54be5df2955df9ee6ff10
Alef gabriel <lafitealef@gmail.com>
2024-03-05 22:27:29 -0300

    up 2

--- src/main/java/br/fiap/com/GerenciadorEscoalar/Controller/AlunoController.java
+++ src/main/java/br/fiap/com/GerenciadorEscoalar/Controller/AlunoController.java
@@ -22,25 +22,83 @@ import br.fiap.com.GerenciadorEscoalar.Model.Aluno;
 @RestController
 @RequestMapping("Aluno")
 public class AlunoController {
-List <Aluno> Repository =  new ArrayList<>();
+List <Aluno> repository =  new ArrayList<>();
+
+Logger log = LoggerFactory.getLogger(getClass());
 
 
     @GetMapping()
     public List<Aluno> index(){
-        return Repository;
+        return repository;
     }
     
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public Aluno AdicionarAluno(@RequestBody Aluno aluno) {
-        System.out.println("esta indo");
-        Repository.add(aluno);
+        log.info("cadastrando aluno" + categoria);
+        repository.add(aluno);
         return aluno;
     }
 
     @GetMapping("{rm}")
-    public Aluno AlunoPorRM(@PathVariable Long rm) {
-        System.out.println("Rm é  {}"+ rm);
+    public ResponseEntity<Categoria> show(@PathVariable Long Rm){
+        log.info("buscar aluno por id {} ", id);
+
+        /*for(Aluno aluno : repository){
+            if (categoria.rm().equals(rm))
+                return ResponseEntity.ok(categoria);
+        }*/
+                                                                     //FindFirst vai pegar o elemento do filtro que passou
+        var alunoEncontrado = repository
+        .stream()
+        .filter( a->  a.rm().equals(rm))
+        .findFirst();
+                                //se estiver vazia
+        if(categoriaEncontrada.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(alunoEncontrado.get() );
+
+        
+
+      
+    }
+    @DeleteMaping("{rm}")
+    public ResponseEntity<Object> destroy(@PAthvAriable Long id){
+        long.info("Apagando categoria com rm {}", rm);
+
+        var alunoEncontrado = repository
+        .stream()
+        .filter( a->  a.rm().equals(rm))
+        .findFirst();
+        if (alunoEncontrado.isEmpty)
+            return ResponseEntity.notFound().build();
+
+        repository.remove(alunoEncontrado.get());
+
+        return responseENtity.noContent().build();
+    }
+    @putMaping("{rm}")
+    public ResponseEntity<Aluno> atualizar (@PathVariable Long rm, @RequestBody Aluno aluno){
+        log.info("Atualizando aluno com rm {} para {}", rm, aluno);
+
+        var alunoEncontrado = repository
+        .stream()
+        .filter( a->  a.rm().equals(rm))
+        .findFirst();
+        if (alunoEncontrado.isEmpty)
+            return ResponseEntity.notFound().build();
+        var AlunoAntigo = alunoEncontrado.get();
+        var AlunoNovo = new Categoria(rm ,aluno.nome(), aluno.turma());
+
+        repository.remove(AlunoAntigo);
+        repository.add(categoriaNova);
+
+        return ResponseEntity
+
+        
+        
+
     }
     
     
--- src/main/java/br/fiap/com/GerenciadorEscoalar/Model/Aluno.java
+++ src/main/java/br/fiap/com/GerenciadorEscoalar/Model/Aluno.java
@@ -4,7 +4,11 @@ package br.fiap.com.GerenciadorEscoalar.Model;
 
 public record Aluno( long Rm, String nome, String turma) {
     
-
+    public Aluno(Long Rm, String nome, String turma){
+        this.Rm = Math.abs( new Random().nextLong() );
+        this.nome = nome;
+        this.turma = turma;
+    }
 
 
     
